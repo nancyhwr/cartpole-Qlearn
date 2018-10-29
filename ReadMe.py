26af6252c23bfdda5827e3353ed4b633ac99ebb2
HannaW <nancywhr@udel.edu>
2018-10-29 17:32:41 -0400

    Update ReadMe.py

--- ReadMe.py
+++ ReadMe.py
@@ -1,9 +1,3 @@
-import gym
-
-
-env = gym.make('CartPole-v0')
-observation  = env.reset()   # return the state
-for i, feature in enumerate(observation):
-	print(i, feature) # i = [0, 1, 2, 3] features: each index's value
-
-observation, reward, done, info = env.step(0)   # make an action and returnt those info
+Q-learning on Cartpole
+Epsilon-exploration 
+10 bins for state discretization
